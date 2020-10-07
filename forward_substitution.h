ec9c2df42b5a134ef2c6f12d997a4df223bbecee
MEGAjosni <jonassoeborgnielsen@gmail.com>
2020-10-07 15:53:54 +0200

    Update forward_substitution.h

--- forward_substitution.h
+++ forward_substitution.h
@@ -1,7 +1,15 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "dot.h"
 
-int fwdsub()
-{
-  printf("testing that this works\n");
-  return 0;
-}
+int fwdsub(
+  unsigned long n,
+  double alpha,
+  double **R,  /* two-dimensional array, row-major */
+  double *b    /* one-dimensional array */
+  ){
+    for (size_t i = 0; i < n; i++)
+    {
+      b[i] -= dot(b, R[i], i) / (alpha + R[i][i]);
+    }
+  }
